4e863dd0f2d0b8a650e6d5b83d828973e7b537ff
Yagnesh Modh <yagnesh.modh@gmail.com>
2016-11-21 23:18:30 +0530

    add carousol

--- js/init.js
+++ js/init.js
@@ -1,11 +1,17 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav({
       edge: 'right'
     });
 
-    $('.carousel').carousel({full_width: true});
+    $('.carousel').carousel({ full_width: true });
+
+    autoplay();
+    function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+    }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
