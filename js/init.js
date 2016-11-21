147de377f3900f2484f0ba310e68b25491f01ab9
Yagnesh Modh <yagnesh.modh@gmail.com>
2016-11-21 23:10:19 +0530

    First commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+      edge: 'right'
+    });
+
+    $('.carousel').carousel({full_width: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
